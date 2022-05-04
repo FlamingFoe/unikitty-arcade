62bd0f6a446bb5e217c6600089a755acce6cc683
FlamingFoe <nehap.ivy@gmail.com>
2022-05-04 17:01:51 +0530

    Added repeated play and score

--- js/RPS.js
+++ js/RPS.js
@@ -1,5 +1,8 @@
-function play()
-{
+let userScore = 0;
+let compScore = 0;
+let username = prompt('Hi, what is your name?');
+let choice = prompt(`Welcome to rock, paper, scissors, ${username}! \n\nHow many times do you want to play?`);
+for (let i = 0; i < choice; i++) {
     let userInput = prompt('Enter R for rock, P for paper or S for scissors:');
     if (userInput !== 'R' && userInput !== 'P' && userInput !== 'S') {
         alert('Invalid input!');
@@ -15,20 +18,23 @@ function play()
         let result = `You chose ${map.get(userInput)}!\n\nComputer chose ${map.get(compInput)}!\n\n`;
         if (userInput === compInput) {
             result += 'It\'s a draw!';
-        } else if (userInput==='R' && compInput==='P' || userInput==='P' && compInput==='S' || userInput==='S' && compInput==='R' ) {
+        } else if (userInput === 'R' && compInput === 'P' || userInput === 'P' && compInput === 'S' || userInput === 'S' && compInput === 'R') {
             result += 'Computer wins!';
-            // return -1;
+            compScore += 1;
         }
         else {
             result += 'You win!';
-            // return 1;
+            userScore += 1;
         }
         alert(result);
     }
+    alert(`Your score: ${userScore}\n\nComputer score: ${compScore}`);
 }
-
-let username = prompt('Hi, what is your name?');
-alert(`Welcome to rock, paper, scissors, ${username}! \n\n`);
-play();
-
-
+if (userScore > compScore) {
+    alert('You win the match!');
+} else if (compScore > userScore) {
+    alert('Computer wins the match!');
+} else {
+    alert('It\'s a draw/!');
+}
+alert('Thanks for playing!\n\nTo play again, just reload!')
